c6f2b272ab1428de51ce03ecd9f8c5a38309b254
Brian Burke <bburke2@binghamton.edu>
2015-11-25 10:11:57 -0500

    Update main.py
    Fixed the problem with entering in individual elements of the matrix by taking input for an entire row and splitting it into individual elements.

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def matrixAdd(matrix1):
+   print("Enter the information of the matrix that will be added to the matrix that you just entered.\n")
+   print("Note that the two matricies to be added must have the same dimensions.\n")
+   matrix2 = getMatrix
+   newMatrix = []
+   for 
+
+def rowInputVerification(i, n):
+    keepGoing = "y"
+    while keepGoing == "y":
+        row = input("Enter the elements in row " + str(i + 1) + ". Separare each element by a single space: ").split()
+        try:
+            for x in range(n):
+                row[x] = int(row[x])
+            if len(row) == n:
+                keepGoing = "n"
+            else:
+                print("Each row can only have as many elements as there are columns in the matrix.")
+                row = input("Enter the elements in row " + str(i + 1) + ". Separare each element by a single space: ").split()
+        except:
+            row = input("ERROR: Enter the elements in row " + str(i + 1) + ". Separare each element by a single space: ").split()
+    return row        
+        
+
 def getColumns():
     
     # Get number of columns (n)
@@ -32,28 +56,18 @@ def getRows():
             rows = input("ERROR: How many rows are in the matrix? \n")
     return m
 
-def getMatrix(choice):
+def getMatrix():
     m = getRows()
     n = getColumns()
     
     matrix = []
 
-    # Getting each element of the matrix
-    
-    for i in range(m):
-        for j in range(n):
-            keepGoing = "y"
-            while keepGoing == "y":
-                
-                ij = input("Enter the element of row " + str(i +1) + " and column " +str(j +1) +": \n")
-                #try:
-                Aij = int(ij)
-                matrix.insert([i][j], Aij)
-                keepGoing = "n"
-                #except:
-#                ij = input("ERROR: Enter the element of row " + str(i +1) + " and column " +str(j +1) +": \n")
+    # Getting each individual row of the matrix
 
-    # Turning matrix list into a nested list (an array of m arrays)
+    for i in range(m):
+        row = rowInputVerification(i, n)
+        matrix.append(row)
+        print("Row " + str(i + 1) + ": " + str(matrix[i]))
     
     print(matrix)
         
@@ -84,6 +98,15 @@ def main():
         except:
             x = input("ERROR: Choose an option (1, 2, or 3): \n")
         
-    getMatrix(choice)
+    matrix1 = getMatrix()
+    
+    if choice == 1:
+        result = matrixAdd(matrix1)
+    elif choice == 2:
+        result = scalarMulti(matrix1)
+    else:
+        result = matrixMult(matrix1)
+
+    print(result)
         
 main()
